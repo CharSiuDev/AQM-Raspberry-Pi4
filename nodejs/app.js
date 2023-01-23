bab43ab90056df746b0cff624166fe8ca01eaf9c
linr2 <ronghui.lin2@mail.dcu.ie>
2023-01-23 20:41:01 +0000

    added updated data collection, transfer and receiving through nodeJS

---
+++ nodejs/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const sqlite3 = require('sqlite3')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+
+app.post('/store', (req, res) => {
+    let data = req.body
+    let tableName = `sensor_data_${data.id}`
+    // store data in SQLite3 database
+    let conn = new sqlite3.Database('data.db')
+    conn.run(`CREATE TABLE IF NOT EXISTS ${tableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, temperature REAL, pressure REAL, humidity REAL, light REAL, oxidised REAL, reduced REAL, nh3 REAL, pm1 REAL, pm25 REAL, pm10 REAL)`, (err) => {
+        if(err){
+            console.log("error creating table")
+        }
+    })
+    conn.run(`INSERT INTO ${tableName} (temperature, pressure, humidity, light, oxidised, reduced, nh3, pm1, pm25, pm10) VALUES (?,?,?,?,?,?,?,?,?,?)`, [data.temperature, data.pressure, data.humidity, data.light, data.oxidised, data.reduced, data.nh3, data.pm1, data.pm25, data.pm10], (err) => {
+        if(err){
+            console.log("error inserting data")
+        }
+    })
+    conn.all(`SELECT count(*) as count FROM ${tableName}`, (err,rows) => {
+        if (err) {
+          console.error(err.message);
+        }
+        if(rows[0].count > 20)
+        conn.run(`DELETE FROM ${tableName} WHERE id = (SELECT id FROM ${tableName} ORDER BY id LIMIT 1)`)
+    });
+    console.log(`Data stored in ${tableName}`)
+    conn.close()
+    res.send("Data stored in SQLite3 database")
+})
---
+++ start.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+import json
+import requests
+import time
+import colorsys
+import sys
+import ST7735
+try:
+    from ltr559 import LTR559
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
+from bme280 import BME280
+from pms5003 import PMS5003, ReadTimeoutError as pmsReadTimeoutError
+from enviroplus import gas
+
+# BME280 temperature/pressure/humidity sensor
+bme280 = BME280()
+# PMS5003 particulate sensor
+pms5003 = PMS5003()
+
+def get_cpu_temp():
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        temp = f.read()
+        temp = int(temp) / 1000.0
+    return temp
+
+factor = 2.25
+cpu_temps = [get_cpu_temp()] * 5
+delay = 0.5
+last_page = 0
+light = 1
+node_ID = 1
+def get_compensated_cpu_temp(cpu_temps):
+        cpu_temp = get_cpu_temp()
+        cpu_temps = cpu_temps[1:] + [cpu_temp]
+        avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+        raw_temp = bme280.get_temperature()
+        temperature_data = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
+        return temperature_data
+
+def getpressure():
+    pressure_data = bme280.get_pressure()
+    return pressure_data
+
+def get_humidity():
+    humidity_data = bme280.get_humidity()
+    return humidity_data
+
+def get_light(proximity):
+        if proximity < 10:
+            light_data = ltr559.get_lux()
+            return light_data
+        else:
+            light_data = 1
+            return light_data
+
+def get_oxidised():
+        data = gas.read_all()
+        oxidised_data = data.oxidising / 1000
+        return oxidised_data
+
+def get_reduced():
+        data = gas.read_all()
+        reduced_data = data.reducing / 1000
+        return reduced_data
+
+def get_nh3():
+        data = gas.read_all()
+        nh3_data = data.nh3 / 1000
+        return nh3_data
+
+def get_pm1():
+        data = pms5003.read()
+        pm1_data = float(data.pm_ug_per_m3(1.0))
+        return pm1_data
+
+def get_pm25():
+        data = pms5003.read()
+        pm25_data = float(data.pm_ug_per_m3(2.5))
+        return pm25_data
+
+def get_pm10():
+        data = pms5003.read()
+        pm10_data = float(data.pm_ug_per_m3(10))
+        return pm10_data
+
+starttime = time.time()
+try:
+    while True:
+        time.sleep(10.0 - ((time.time() - starttime) % 10.0))
+        proximity = ltr559.get_proximity()
+        temperature_data = get_compensated_cpu_temp(cpu_temps)
+        pressure_data = getpressure()
+        humidity_data = get_humidity()
+        light_data = get_light(proximity)
+        oxidised_data = get_oxidised()
+        reduced_data = get_reduced()
+        nh3_data = get_nh3()
+        pm1_data = get_pm1()
+        pm25_data = get_pm25()
+        pm10_data = get_pm10()
+
+        data = {
+         "id": node_ID,
+         "temperature": temperature_data,
+         "pressure": pressure_data,
+         "humidity": humidity_data,
+         "light": light_data,
+         "oxidised": oxidised_data,
+         "reduced": reduced_data,
+         "nh3": nh3_data,
+         "pm1": pm1_data,
+         "pm25": pm25_data,
+         "pm10": pm10_data}
+        
+        url = 'http://192.168.0.59:3000/store'
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(data), headers = headers)
+        if response.status_code == 200:
+            print('Success')
+        else:
+            print('Error')
+
+
+# Exit cleanly
+except KeyboardInterrupt:
+    sys.exit(0)
