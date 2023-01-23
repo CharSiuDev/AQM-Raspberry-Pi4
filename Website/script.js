fe4ccf0e3b97cbffe2c298f79765e72da31ed6b9
Vaidas Buzas <vaidas.buzas2@mail.dcu.ie>
2023-01-23 21:34:52 +0000

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -1,8 +1,22 @@
-// Fetch sensor data from the server
-fetch('/sensor-data')
-  .then(response => response.json())
-  .then(data => {
-    // Update the data div with the sensor data
-    document.getElementById('data').innerHTML = data.value;
-  })
-  .catch(error => console.error(error));
+const data = {
+    id: 1,
+    temperature: 20,
+    pressure: 1013,
+    humidity: 40,
+    light: 400,
+    oxidised: 2,
+    reduced: 3,
+    nh3: 0.5,
+    pm1: 12,
+    pm25: 34,
+    pm10: 56
+}
+
+fetch('http://local-ip-address/store', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' }
+})
+.then(res => res.json())
+.then(response => console.log('Success:', response))
+.catch(error => console.error('Error:', error));
