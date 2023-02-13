a2e26804c52470253ee3e8654315565a0d3d4ac1
linr2 <ronghui.lin2@mail.dcu.ie>
2023-02-13 13:24:56 +0000

    Added email notification functionality

--- Website/server.js
+++ Website/server.js
@@ -4,6 +4,31 @@ const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser')
 const session = require('express-session');
 const bcrypt = require('bcrypt');
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+async function sendEmail(recipient, subject, body) {
+  // create a nodemailer transporter using SMTP
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: process.env.EMAIL_USERNAME, // sender address
+    to: recipient, // list of receivers
+    subject: subject, // Subject line
+    text: body, // plain text body
+  });
+
+  console.log(`Message sent: ${info.messageId}`);
+}
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -41,27 +66,28 @@ app.get('/register', (req, res) => {
 });  
 
 app.post('/register', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-  
-    bcrypt.hash(password, 10, (err, hash) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  let email = req.body.email;
+
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error encrypting password');
+    }
+    // Store the username and encrypted password in a database
+    let conn = new sqlite3.Database('data.db');
+    let sql = `INSERT INTO users (username, email, password) VALUES (?,?,?)`;
+    conn.run(sql, [username, email, hash], (err) => {
       if (err) {
         console.error(err);
-        res.status(500).send('Error encrypting password');
-      }
-      // Store the username and encrypted password in a database
-      let conn = new sqlite3.Database('data.db');
-      let sql = `INSERT INTO users (username, password) VALUES (?,?)`;
-      conn.run(sql, [username, hash], (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Error registering user');
-        }
-        res.send('User registered successfully');
+        res.status(500).send('Error registering user');
+      } else {
         res.redirect('/');
-      });
+      }
       conn.close();
     });
+  });
 });
 
 app.get('/login', (req, res) => {
@@ -78,7 +104,7 @@ app.post('/login', (req, res) => {
   
     // Retrieve the encrypted password from the database
     let conn = new sqlite3.Database('data.db');
-    let sql = `SELECT password FROM users WHERE username = ?`;
+    let sql = `SELECT email, password FROM users WHERE username = ?`;
     conn.get(sql, [username], (err, row) => {
       if (err) {
         console.error(err);
@@ -89,7 +115,8 @@ app.post('/login', (req, res) => {
         return;
       }
       let hash = row.password;
-  
+      let email = row.email;
+
       // Compare the provided password with the encrypted password
       bcrypt.compare(password, hash, (err, result) => {
         if (err) {
@@ -97,8 +124,9 @@ app.post('/login', (req, res) => {
           res.status(500).send('Error checking password');
         }
         if (result) {
-          // Store the username in a session
+          // Store the username and email in a session
           req.session.username = username;
+          req.session.email = email;
           res.redirect('/');
         } else {
           res.status(401).send('Incorrect password');
@@ -148,31 +176,125 @@ app.get('/data', (req, res) => {
     }
 });
 
+app.post('/create-notification', (req, res) => {
+  let notificationEmail = req.session.email;
+  let temperature = req.body.temperature;
+  let pressure = req.body.pressure;
+  let humidity = req.body.humidity;
+  let light = req.body.light;
+  let oxidised = req.body.oxidised;
+  let reduced = req.body.reduced;
+  let nh3 = req.body.nh3;
+  let pm1 = req.body.pm1;
+  let pm25 = req.body.pm25;
+  let pm10 = req.body.pm10;
+
+  let conn = new sqlite3.Database('data.db');
+  let sql = `INSERT INTO email_notifications (email, temperature, pressure, humidity, light, oxidised, reduced, nh3, pm1, pm25, pm10, email_sent) 
+              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)`;
+  conn.run(sql, [notificationEmail, temperature, pressure, humidity, light, oxidised, reduced, nh3, pm1, pm25, pm10], (err) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('Error creating notification');
+          return;
+      }
+      res.redirect('/data');
+  });
+  conn.close();
+});
+
 app.post('/store', (req, res) => {
-    let data = req.body
-    let tableName = `sensor_data_1`
-    // store data in SQLite3 database
-    let conn = new sqlite3.Database('data.db')
-    conn.run(`CREATE TABLE IF NOT EXISTS ${tableName} (time REAL, temperature REAL, pressure REAL, humidity REAL, light REAL, oxidised REAL, reduced REAL, nh3 REAL, pm1 REAL, pm25 REAL, pm10 REAL)`, (err) => {
-        if(err){
-            console.log("error creating table")
-        }
-    })
-    conn.run(`INSERT INTO ${tableName} (time, temperature, pressure, humidity, light, oxidised, reduced, nh3, pm1, pm25, pm10) VALUES (?,?,?,?,?,?,?,?,?,?,?)`, [data.time, data.temperature, data.pressure, data.humidity, data.light, data.oxidised, data.reduced, data.nh3, data.pm1, data.pm25, data.pm10], (err) => {
-        if(err){
-            console.log("error inserting data")
-        }
-    })
-    conn.all(`SELECT count(*) as count FROM ${tableName}`, (err,rows) => {
+  let data = req.body;
+  let tableName = `sensor_data_1`;
+
+  // Store data in SQLite3 database
+  let conn = new sqlite3.Database('data.db');
+  conn.run(`CREATE TABLE IF NOT EXISTS ${tableName} (time REAL, temperature REAL, pressure REAL, humidity REAL, light REAL, oxidised REAL, reduced REAL, nh3 REAL, pm1 REAL, pm25 REAL, pm10 REAL)`, (err) => {
+    if (err) {
+      console.log("error creating table")
+    }
+  });
+
+  conn.run(`INSERT INTO ${tableName} (time, temperature, pressure, humidity, light, oxidised, reduced, nh3, pm1, pm25, pm10) VALUES (?,?,?,?,?,?,?,?,?,?,?)`, [data.time, data.temperature, data.pressure, data.humidity, data.light, data.oxidised, data.reduced, data.nh3, data.pm1, data.pm25, data.pm10], (err) => {
+    if (err) {
+      console.log("error inserting data")
+    }
+  });
+
+  conn.all(`SELECT * FROM email_notifications`, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+
+    let notificationsToRemove = [];
+
+    rows.forEach((row) => {
+      let exceedData = [];
+
+      if (row.temperature && data.temperature > row.temperature) {
+        exceedData.push("Temperature");
+      }
+      if (row.pressure && data.pressure > row.pressure) {
+        exceedData.push("Pressure");
+      }
+      if (row.humidity && data.humidity > row.humidity) {
+        exceedData.push("Humidity");
+      }
+      if (row.light && data.light > row.light) {
+        exceedData.push("Light");
+      }
+      if (row.light && data.light > row.light) {
+        exceedData.push("Oxidised");
+      }
+      if (row.light && data.light > row.light) {
+        exceedData.push("Reduced");
+      }
+      if (row.light && data.light > row.light) {
+        exceedData.push("NH3");
+      }
+      if (row.light && data.light > row.light) {
+        exceedData.push("PM1");
+      }
+      if (row.light && data.light > row.light) {
+        exceedData.push("PM25");
+      }
+      if (row.light && data.light > row.light) {
+        exceedData.push("PM10");
+      }
+
+      // If any data exceeds the threshold, send email
+      if (exceedData.length > 0) {
+        let email = row.email;
+        let subject = "Sensor Data Exceeds Threshold";
+        let message = `The following data exceeds the threshold:\n${exceedData.join(', ')}\n\nSensor data:\n${JSON.stringify(data, null, 2)}`;
+
+        sendEmail(email, subject, message);
+        notificationsToRemove.push(row.id);
+      }
+    });
+
+    // Remove the notification requests that have been sent
+    notificationsToRemove.forEach((id) => {
+      conn.run(`DELETE FROM email_notifications WHERE id = ?`, [id], (err) => {
         if (err) {
           console.error(err.message);
         }
-        if(rows[0].count > 20)
-        conn.run(`DELETE FROM ${tableName} WHERE time = (SELECT time FROM ${tableName} ORDER BY time LIMIT 1)`)
+      });
     });
-    console.log(`Data stored in ${tableName}`)
-    conn.close()
-    res.send("Data stored in SQLite3 database")
-})
+  });
+
+  conn.all(`SELECT count(*) as count FROM ${tableName}`, (err,rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+    if(rows[0].count > 20)
+    conn.run(`DELETE FROM ${tableName} WHERE time = (SELECT time FROM ${tableName} ORDER BY time LIMIT 1)`)
+});
+
+
+  console.log(`Data stored in ${tableName}`);
+  conn.close();
+  res.send("Data stored in SQLite3 database");
+});
 app.listen(3000, () => {
-    console.log("Server running on port 3000")});
+  console.log("Server running on port 3000")});
--- start.py
+++ start.py
@@ -2,6 +2,7 @@
 import json
 import requests
 import time
+from datetime import datetime
 import colorsys
 import sys
 import ST7735
@@ -37,19 +38,19 @@ def get_compensated_cpu_temp(cpu_temps):
         avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
         raw_temp = bme280.get_temperature()
         temperature_data = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
-        return temperature_data
+        return round(temperature_data,2)
 
 def getpressure():
-    pressure_data = bme280.get_pressure()
+    pressure_data = round(bme280.get_pressure(),2)
     return pressure_data
 
 def get_humidity():
-    humidity_data = bme280.get_humidity()
+    humidity_data = round(bme280.get_humidity(),2)
     return humidity_data
 
 def get_light(proximity):
         if proximity < 10:
-            light_data = ltr559.get_lux()
+            light_data = round(ltr559.get_lux(),2)
             return light_data
         else:
             light_data = 1
@@ -57,32 +58,32 @@ def get_light(proximity):
 
 def get_oxidised():
         data = gas.read_all()
-        oxidised_data = data.oxidising / 1000
+        oxidised_data = round(data.oxidising / 1000,2)
         return oxidised_data
 
 def get_reduced():
         data = gas.read_all()
-        reduced_data = data.reducing / 1000
+        reduced_data = round(data.reducing / 1000,2)
         return reduced_data
 
 def get_nh3():
         data = gas.read_all()
-        nh3_data = data.nh3 / 1000
+        nh3_data = round(data.nh3 / 1000,2)
         return nh3_data
 
 def get_pm1():
         data = pms5003.read()
-        pm1_data = float(data.pm_ug_per_m3(1.0))
+        pm1_data = data.pm_ug_per_m3(1.0)
         return pm1_data
 
 def get_pm25():
         data = pms5003.read()
-        pm25_data = float(data.pm_ug_per_m3(2.5))
+        pm25_data = data.pm_ug_per_m3(2.5)
         return pm25_data
 
 def get_pm10():
         data = pms5003.read()
-        pm10_data = float(data.pm_ug_per_m3(10))
+        pm10_data = data.pm_ug_per_m3(10),
         return pm10_data
 
 starttime = time.time()
@@ -100,9 +101,11 @@ try:
         pm1_data = get_pm1()
         pm25_data = get_pm25()
         pm10_data = get_pm10()
+        currtime = datetime.now().strftime("%H:%M:%S")
+        print(currtime)
 
         data = {
-         "id": node_ID,
+         "time": currtime,
          "temperature": temperature_data,
          "pressure": pressure_data,
          "humidity": humidity_data,
